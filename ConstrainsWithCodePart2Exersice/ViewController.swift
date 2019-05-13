dd359e3e5c01f0d461740f31b4b81d801a93dd78
Art Karma <inste@icloud.com>
2019-05-13 20:13:55 +0300

    Initial Commit

---
+++ ConstrainsWithCodePart2Exersice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConstrainsWithCodePart2Exersice
+//
+//  Created by Art Karma on 5/13/19.
+//  Copyright © 2019 Art Karma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
